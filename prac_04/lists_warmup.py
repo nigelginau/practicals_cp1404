96e85c35dadf1d14a5a562a8a7a671449516d3a6
jc262760 <nigel.ginai@my.jcu.edu.au>
2019-03-29 10:10:58 +1000

    Wrote Python expressions to manipulate a list

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Change the first element of numbers to "ten"
+numbers[0] = 10
+print(numbers)
+
+# Change the last element of numbers to 1
+numbers[-1] = 1
+print(numbers)
+
+# Get all the elements from numbers except the first two
+print(numbers[2:])
+
+# Check if 9 is an element of numbers
+print(9 in numbers)
