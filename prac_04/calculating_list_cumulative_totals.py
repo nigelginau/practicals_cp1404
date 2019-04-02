5d1055e4485c56966bc5e89e93422077265780e4
jc262760 <nigel.ginau@my.jcu.edu.au>
2019-04-02 16:43:18 +1000

    Refactored the functions in the script

--- prac_04/calculating_list_cumulative_totals.py
+++ prac_04/calculating_list_cumulative_totals.py
@@ -1,12 +1,15 @@
-# Ask  for number of monthly incomes to enter
-total_months = int(input('How many months ? '))
-cumulative_total = [0]
-# Get and store the monthly incomes in a list
-for i in range(1, total_months + 1):
-    income_for_month = [int(input('Enter income for month {}:'.format(i)))]
-    cumulative_total = income_for_month + cumulative_total
+def main():
+    # Ask  for number of monthly incomes to enter
+    total_months = int(input('How many months ? '))
+    income_list = [0]
+    # Get and store the monthly incomes in a list
+    for i in range(1, total_months + 1):
+        income_for_month = [int(input('Enter income for month {}:'.format(i)))]
+        income_list = income_list + income_for_month
 
     print("\nIncome Report\n-------------")
 
     for i in range(1, total_months + 1):
-        print("Month {0} - Income: $  {1:10.2f}          Total: $ {1:10.2f}".format(i, cumulative_total[i - 1]))
+        cumulative_total = sum(income_list[:i + 1])
+        print("Month {0} - Income: $  {1:10.2f}          Total: $ {2:10.2f}".format(i, income_list[i], cumulative_total))
+main()
