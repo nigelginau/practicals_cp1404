2bf71ee8d50ac8fe749e83a34c4dab4cf4b973f2
jc262760 <nigel.ginai@my.jcu.edu.au>
2019-04-05 10:55:20 +1000

    Added hex colour dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+hex_colours_dict = {"aquamarine4": "#458b74", "black": "#000000", "blue2": "#0000ee", "chartreuse2": "#76ee00",
+                    "DarkOrchid3": '#9a32cd', "DarkOrange2": "#ff7f00", "firebrick2": "#ee2c2c", "gold3": "gray25",
+                    'green3': '#00cd00', "HotPink1": '#ff6eb4'}
+
+print(
